58121da8d620f0082afa7deb2514b269cc401e16
Christian Baranowski <tux2323@pro-pix.de>
2010-03-07 10:42:08 +0100

    YML Configuration prototyp add to code for the basar configuration.

---
+++ kasse/src/test/java/basar/configuration/YmlTest.java
@@ -0,0 +1,45 @@
+package basar.configuration;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import org.junit.Test;
+import org.yaml.snakeyaml.JavaBeanLoader;
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.Yaml;
+
+import basar.config.Configuration;
+import basar.config.Database;
+import basar.config.Kasse;
+
+public class YmlTest {
+
+
+	@Test
+	public void testReadConfig() throws Exception {
+		URL resource = getClass().getResource("/configuration.yml");
+		assertNotNull(resource);
+		
+		InputStream ins = resource.openStream();
+		Yaml yaml = new Yaml();
+		
+		TypeDescription typeDescription = new TypeDescription(Configuration.class, "!configuration");
+		typeDescription.putMapPropertyType("database", Database.class, String.class);
+		JavaBeanLoader<Configuration> loader = 
+			new JavaBeanLoader<Configuration>(typeDescription);
+		
+		Configuration configuration = loader.load(ins);
+		
+		Database database = configuration.getDatabase();
+		assertEquals("root", database.getUsername());
+		assertEquals("test", database.getPassword());
+		
+		Kasse kasse = configuration.getKasse();
+		assertEquals("fabienneKasse", kasse.getName());
+		assertEquals(20, kasse.getProzent());
+	}
+	
+	
+}
