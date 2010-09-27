cf9e0b417cde0ae8558488efeeea6edbddf04e69
tux2323 <developer@noname>
2010-09-27 22:02:14 +0200

    Add Document as scala class

---
+++ kasse/src/main/scala/basar/domain/Document.scala
@@ -0,0 +1,40 @@
+package basar.domain
+
+import java.io.Serializable
+import java.util.List
+import java.util.ArrayList
+
+import scala.reflect._
+
+class Document extends Serializable {
+
+  @BeanProperty
+  var basarNumber : Long = -1L
+
+  @BeanProperty
+  var name = "-"
+
+  @BeanProperty
+  val positions: List[DocumentPosition] = new ArrayList[DocumentPosition]
+
+  @BeanProperty
+  var summe = ""
+
+  @BeanProperty
+  var rate = ""
+
+  @BeanProperty
+  var procceeds = ""
+
+  @BeanProperty
+  var prozent: String = null
+  
+  def addPosition(position : DocumentPosition) = {
+	  positions.add(position)
+  }
+  
+  def removePosition(position : DocumentPosition) = {
+	  positions.remove(position)
+  }
+
+}
