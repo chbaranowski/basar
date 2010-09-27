f09a53056471c10ee2f5e621dc8e24ad3f719c02
tux2323 <developer@noname>
2010-09-27 22:07:06 +0200

    Add Document Position as scala class.

---
+++ kasse/src/main/scala/basar/domain/DocumentPosition.scala
@@ -0,0 +1,16 @@
+package basar.domain
+
+import scala.reflect._
+
+class DocumentPosition {
+
+  @BeanProperty
+  var position = -1
+
+  @BeanProperty
+  var price = ""
+
+  @BeanProperty
+  var description = ""
+	  
+}
